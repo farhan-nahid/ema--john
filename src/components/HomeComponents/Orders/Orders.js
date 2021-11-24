2c17a68ffa7f9c48972d6acb8527ca8291d2b679
gesora96 <nahidahmed9933@gmail.com>
2021-11-24 23:25:18 +0600

    filtered orders by email added

--- src/components/HomeComponents/Orders/Orders.js
+++ src/components/HomeComponents/Orders/Orders.js
@@ -1,24 +1,30 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const { loggedInUser } = useAuth();
 
   useEffect(() => {
     axios
-      .get('http://localhost:5000/all-orders')
+      .get(`http://localhost:5000/all-orders?email=${loggedInUser.email}`)
       .then((res) => setOrders(res.data))
       .catch((err) => console.log(err));
-  }, []);
+  }, [loggedInUser]);
+
   return (
-    <div>
+    <section className='container'>
       <h1>Order Page</h1>
+      <h3>You have Placed {orders.length}</h3>
       <ul>
         {orders.map((order) => (
-          <li key={order._id}>{order.email}</li>
+          <li key={order._id}>
+            {order.name} {order.email}
+          </li>
         ))}
       </ul>
-    </div>
+    </section>
   );
 };
 
